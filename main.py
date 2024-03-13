dc020bab629f7d4e2f4430d898459d1cefa2c551
ricky-rinaldo <ricky.rinaldo80@gmail.com>
2024-03-14 04:05:07 +0700

    MODULARISASI dengan FUNCTION

--- main.py
+++ main.py
@@ -2,43 +2,10 @@
 Aplikasi deteksi gempa
 MODULARISASI dengan FUNCTION
 '''
-
-
-def ekstraksi_data():
-    '''
-    tanggal: 13 Maret 2024
-    waktu: 23:39:21 WIB
-    magnitudo: 5.2
-    kedalaman: 182 km
-    lokasi: LS 6.58, BT 129.99
-    pusat gempa: 212 km BaratLaut TANIMBAR
-    keterangan: tidak berpotensi TSUNAMI
-    :return:
-    '''
-    hasil = dict()
-    hasil['tanggal']= '13 Maret 2024'
-    hasil['waktu']= '23:39:21 WIB'
-    hasil['magnitudo']= 5.2
-    hasil['kedalaman']= '182 km'
-    hasil['lokasi']= {'ls':6.58, 'bt':129.99}
-    hasil['pusat_gempa']= '212 km Barat Laut TANIMBAR'
-    hasil['keterangan']= 'tidak berpotensi TSUNAMI'
-    return hasil
-
-
-def tampilkan_data(result):
-    print('Gempa terakhir berdasarkan BMKG :')
-    print(f"Tanggal {result['tanggal']}")
-    print(f"Waktu {result['waktu']}")
-    print(f"Magnitudo {result['magnitudo']}")
-    print(f"Lokasi : LS = {result['lokasi']['ls']}, BT = {result['lokasi']['bt']}")
-    print(f"Kedalaman {result['kedalaman']}")
-    print(f"Pusat Gempa {result['pusat_gempa']}")
-    print(f"Keterangan {result['keterangan']}")
-
+import gempa_terkini
 
 if __name__ == '__main__':
     print ('Aplikasi utama')
-    result = ekstraksi_data()
-    tampilkan_data(result)
+    result = gempa_terkini.ekstraksi_data()
+    gempa_terkini.tampilkan_data(result)
     
